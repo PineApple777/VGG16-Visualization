b2ef2e1f4ee5fafa5bbce0bf36a0f158b9312c9c
PineApple777 <metaljsw2@naver.com>
2019-08-06 20:09:57 +0900

    code repacked extract perlayer

--- get_weights.py
+++ get_weights.py
@@ -17,17 +17,34 @@ def traverse_datasets(hdf_file):
         for path, _ in h5py_dataset_iterator(f):
             yield path
 
+def extract_file_perepoch(epoch, epochFolderPath, layerFolderPath):
+    for num in range(1, epoch+1):
+        filename = epochFolderPath + f"\\cifar10-weights-epoch{num:02d}.hdf5"
+        filedir = layerFolderPath + f"epoch{num:02d}\\"
+        if not(os.path.isdir(filedir)):
+            os.makedirs(os.path.join(filedir))
+        with h5py.File(filename, 'r') as f:
+            for dset in traverse_datasets(filename):
+                ids = dset.split('/')[3]
+                textfile = filedir + f"cifar10-e{num:02d}-{dset.split('/')[2]}_{ids.split(':')[0]}.txt"
+                print(textfile)
+                text = open(textfile, 'a')
+                text.write('Path:' + str(dset) + '\n')
+                text.write('Shape:'+ str(f[dset].shape) + '\n')
+                text.write('Data type:' + str(f[dset].dtype) + '\n')
+                arr = f[dset][:]
+                text.write(str(arr.tolist()) + '\n')
+                text.close()
 
-for num in range(1, 250):
-    filename = f"D:\\extract\\perepoch\\cifar10-weights-epoch{num:02d}.hdf5"
-    if not(os.path.isdir(f"D:\\extract\\perlayer\\epoch{num:02d}")):
-        os.makedirs(os.path.join(f"D:\\extract\\perlayer\\epoch{num:02d}"))
-    with h5py.File(filename, 'r') as f:
-        for dset in traverse_datasets(filename):
+def extract_file_final(finalPath, outputFolder):
+    if not(os.path.isdir(outputFolder)):
+        os.makedirs(os.path.join(outputFolder))
+    with h5py.File(finalPath, 'r') as f:
+        for dset in traverse_datasets(finalPath):
             ids = dset.split('/')[3]
-            textfile = f"D:\\extract\\perlayer\\epoch{num:02d}\\cifar10-e{num:02d}-{dset.split('/')[2]}_{ids.split(':')[0]}.txt"
+            textfile = outputFolder + f"cifar10-f-{dset.split('/')[2]}_{ids.split(':')[0]}.txt"
             print(textfile)
-            text = open(textfile, 'a')
+            text = open(textfile, 'w')
             text.write('Path:' + str(dset) + '\n')
             text.write('Shape:'+ str(f[dset].shape) + '\n')
             text.write('Data type:' + str(f[dset].dtype) + '\n')
@@ -35,19 +52,10 @@ for num in range(1, 250):
             text.write(str(arr.tolist()) + '\n')
             text.close()
 
+def visualization():
+    return
 
-finalPath = "cifar10vgg.h5"
-if not(os.path.isdir(f"D:\\extract\\perlayer\\final")):
-    os.makedirs(os.path.join(f"D:\\extract\\perlayer\\final"))
-with h5py.File(finalPath, 'r') as f:
-    for dset in traverse_datasets(finalPath):
-        ids = dset.split('/')[3]
-        textfile = f"D:\\extract\\perlayer\\final\\cifar10-f-{dset.split('/')[2]}_{ids.split(':')[0]}.txt"
-        print(textfile)
-        text = open(textfile, 'w')
-        text.write('Path:' + str(dset) + '\n')
-        text.write('Shape:'+ str(f[dset].shape) + '\n')
-        text.write('Data type:' + str(f[dset].dtype) + '\n')
-        arr = f[dset][:]
-        text.write(str(arr.tolist()) + '\n')
-        text.close()
+
+if __name__ == "__main__":
+    extract_file_perepoch(250, f"D:\\extract\\perepoch\\", f"D:\\extract\\perlayer\\")
+    extract_file_final("cifar10vgg.h5", f"D:\\extract\\perlayer\\final\\")
